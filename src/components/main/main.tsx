694fb85e574cb16ec87dda39fb71c656191900e8
Saullo Reis <saulloreis01@hotmail.com>
2023-02-15 19:18:04 -0300

    criado a página principal

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -1,9 +1,20 @@
-import { Agents } from "./agents/agents"
+import './styles/main.sass'
+import { Link } from 'react-router-dom'
 
 export const Main = () => {
-    return(
-        <section>
-            <Agents/>
-        </section>
-    )
+
+    return (
+      <section className="main">
+        <h2 className="main-text">
+          Aqui você encontra todos os campeões do valorant e todas as armas, e
+          também temos um quiz para você práticar para saber tudo sobre
+          valorant.
+        </h2>
+        <nav className='main-buttons'>
+          <Link to={"/agentes"}>Agentes</Link>
+          <Link to={"/"}>Armas</Link>
+          <Link to={"/"}>Quiz</Link>
+        </nav>
+      </section>
+    );
 }
