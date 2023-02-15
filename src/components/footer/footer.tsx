0966539a786ced9771e45bb8d3068af07f9aafdb
Saullo Reis <saulloreis01@hotmail.com>
2023-02-15 19:14:21 -0300

    footer feito

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import "./styles/styles.sass";
+
+export const Footer = () => {
+  return (
+    <section className="footer">
+      <p className="footer-text">
+        Saullo Reis &copy; 2023
+      </p>
+    </section>
+  );
+};
