298232693884210d7d18fe9f0c5ba42301f916fe
Saullo Reis <saulloreis01@hotmail.com>
2023-02-16 18:42:12 -0300

    adicionando rota dos detalhes

--- src/components/main/pages.tsx
+++ src/components/main/pages.tsx
@@ -1,12 +1,15 @@
-import { Agents } from "./agents/agents"
-import { Route, Routes,} from 'react-router-dom'
-import { Main } from "./main"
+import { AgentDetails } from "./agents/agentDetails";
+import { Agents } from "./agents/agents";
+import { Route, Routes } from "react-router-dom";
+import { Main } from "./main";
+import { Footer } from "../footer/footer";
 
 export const Pages = () => {
-    return(
-        <Routes>
-            <Route path="/" element={<Main/>}></Route>
-            <Route path='/agentes' element={<Agents/>}></Route>
-        </Routes>
-    )
-}
+  return (
+    <Routes>
+      <Route path="/" element={<Main />}></Route>
+      <Route path="/agentes" element={<Agents />}></Route>
+      <Route path="/agente/:id" element={<AgentDetails />}></Route>
+    </Routes>
+  );
+};
