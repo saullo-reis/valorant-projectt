93d253b982da1e2d966d9caa45a2d10b6e5bfa7e
Saullo Reis <saulloreis01@hotmail.com>
2023-03-09 09:47:43 -0300

    testes finalizados em menu quiz

--- src/components/main/quiz/quiz-menu.test.tsx
+++ src/components/main/quiz/quiz-menu.test.tsx
@@ -6,35 +6,61 @@ import store from "../../store";
 import "@testing-library/jest-dom";
 import { QuizMenu } from "./quiz-menu";
 
-const handleClickEasy = jest.fn();
+const setModeSpy = jest.fn();
 
 const renderComponent = () => {
   render(
     <Provider store={store}>
       <BrowserRouter>
-        <QuizMenu />
+        <QuizMenu setModeSpy={setModeSpy} />
       </BrowserRouter>
     </Provider>
   );
 };
 
-
 describe("Quiz menu test", () => {
   it("should be render the modes", () => {
     renderComponent();
 
-    expect(screen.getByText("Iniciar")).toBeInTheDocument();
     expect(screen.getByText("Fácil")).toBeInTheDocument();
     expect(screen.getByText("Médio")).toBeInTheDocument();
     expect(screen.getByText("Difícil")).toBeInTheDocument();
   });
 
-  it("should modify the variable for easy", () => { 
+  it("should change the variable to easy", () => {
     renderComponent();
 
     const buttonEasy = screen.getByText("Fácil");
-    fireEvent.click(buttonEasy, { target: { value: "easy" } });
+    fireEvent.click(buttonEasy);
 
-    expect(handleClickEasy).toBe("easy");
+    expect(setModeSpy).toHaveBeenCalledWith("easy");
+  });
+
+  it("should modify the variable for medium", () => {
+    renderComponent();
+
+    const buttonMedium = screen.getByText("Médio");
+    fireEvent.click(buttonMedium);
+
+    expect(setModeSpy).toHaveBeenCalledWith("medium");
+  });
+
+  it("should change the variable to hard", () => {
+    renderComponent();
+
+    const buttonHard = screen.getByText("Difícil");
+    fireEvent.click(buttonHard);
+
+    expect(setModeSpy).toHaveBeenCalledWith("hard");
+  });
+
+  it("should modify the page after clicking a mode", () => {
+    renderComponent();
+
+    const buttonHard = screen.getByText("Difícil");
+    fireEvent.click(buttonHard);
+    const buttonStart = screen.getByText("Iniciar");
+
+    expect(buttonStart).toHaveAttribute("href", "/quiz/start");
   });
 });
